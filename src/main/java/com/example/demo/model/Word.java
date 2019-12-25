a53bcc9cc594616a255f55465d29d51ae78d5f37
denemebulut123 <59229283+denemebulut123@users.noreply.github.com>
2019-12-25 19:19:51 +0300

    en zor kısım

---
+++ src/main/java/com/example/demo/model/Word.java
@@ -0,0 +1,43 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Word {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+
+	@Column
+	private String word;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getWord() {
+		return word;
+	}
+
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+	
+	
+}
